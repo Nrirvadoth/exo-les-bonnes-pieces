0ab839498db082ded98e7cee97d42edcc0d35bf2
Mat <mathurin.malandain+github@gmail.com>
2023-08-30 17:57:59 +0200

    P2C2 - exo

--- pieces.js
+++ pieces.js
@@ -34,8 +34,9 @@ for (let i = 0; i < pieces.length; i++) {
     pieceElement.appendChild(descriptionElement);
     pieceElement.appendChild(stockElement);
 
-}
+ }
  
+ //gestion des boutons 
 const boutonTrier = document.querySelector(".btn-trier");
 
 boutonTrier.addEventListener("click", function () {
@@ -49,27 +50,44 @@ boutonTrier.addEventListener("click", function () {
 const boutonFiltrer = document.querySelector(".btn-filtrer");
 
 boutonFiltrer.addEventListener("click", function () {
-   const piecesFiltrees = pieces.filter(function (piece) {
-       return piece.prix <= 35;
-   });
-   console.log(piecesFiltrees);
-}) ;
-
-const boutonTrierDecroissant = document.querySelector(".btn-trier-decroissant");
-
-boutonTrierDecroissant.addEventListener("click", function () {
-    const piecesOrdonneesDecroissant = Array.from(pieces);
-    piecesOrdonneesDecroissant.sort(function (a, b) {
-        return b.prix - a.prix;
-     });
-     console.log(piecesOrdonneesDecroissant);
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.prix <= 35;
+    });
+   console.log(piecesFiltrees)
 });
 
-const boutonFiltrerDescription = document.querySelector(".btn-filtre-description");
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+boutonDecroissant.addEventListener("click", function () {
+    const piecesOrdonnees = Array.from(pieces);
+    piecesOrdonnees.sort(function (a, b) {
+        return b.prix - a.prix;
+     });
+     console.log(piecesOrdonnees);
+});
 
-boutonFiltrerDescription.addEventListener("click", function () {
-   const piecesDecrites = pieces.filter(function (piece) {
-       return piece.description;
-   });
-   console.log(piecesDecrites);
-}) ;
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+boutonNoDescription.addEventListener("click", function () {
+    const piecesFiltrees = pieces.filter(function (piece) {
+        return piece.description;
+    });
+   console.log(piecesFiltrees);
+});
+
+const noms = pieces.map(piece => piece.nom);
+for(let i = pieces.length -1 ; i >= 0; i--){
+    if(pieces[i].prix > 35){
+        noms.splice(i,1);
+    }
+}
+
+//Création de la liste
+const abordablesElements = document.createElement('ul');
+//Ajout de chaque nom à la liste
+for(let i=0; i < noms.length ; i++){
+    const nomElement = document.createElement('li');
+    nomElement.innerText = noms[i];
+    abordablesElements.appendChild(nomElement)
+}
+// Ajout de l'en-tête puis de la liste au bloc résultats filtres
+document.querySelector('.abordables')
+    .appendChild(abordablesElements)
